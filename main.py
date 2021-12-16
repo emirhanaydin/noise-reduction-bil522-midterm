63f944b7dad72265ba01e5143c62ec1e2054c918
emirhanaydin <emirhanaydin95@gmail.com>
2021-12-16 16:03:39 +0300

    add reading the input image routine

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 import sys
+from pathlib import Path
+
+import cv2 as cv
+
+
+def print_fatal(s: str):
+    sys.stderr.write(s)
+    sys.exit(1)
 
 
 def validate_version():
     major, minor = sys.version_info[0:2]
     if major < 3 or minor < 10:
-        sys.stderr.write(f"Python interpreter must be {major}.{minor} or greater.")
+        print_fatal(f"Python interpreter must be {major}.{minor} or greater.")
+
+
+def validate_path(path: Path):
+    if not path.is_file():
+        print_fatal("Specified file path is invalid.")
+
+
+def get_input_path() -> Path:
+    argv = sys.argv
+    if len(argv) < 2:
+        print_fatal(f"Input file path must be specified as command line argument.")
+
+    input_path = Path(argv[1])
+    validate_path(input_path)
+    return input_path
+
+
+def get_input_image():
+    input_path = get_input_path()
+    return cv.imread(str(input_path))
+
+
+def main():
+    validate_version()
+    img = get_input_image()
+
+    cv.imshow("input", img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    validate_version()
+    main()
